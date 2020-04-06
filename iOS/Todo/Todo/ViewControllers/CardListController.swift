b8be3bc21cd319fce9e2dda079fe4118f22b8ff8
ehgud0670 <kimdo2297@gmail.com>
2020-04-06 22:37:10 +0900

    feat: 타이틀 뷰 안에 라벨과 추가 버튼을 추가하고 오토레이아웃을 적용함

--- iOS/Todo/Todo/ViewControllers/CardListController.swift
+++ iOS/Todo/Todo/ViewControllers/CardListController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 final class CardListController: UIViewController {
+    private let titleView : TitleView = {
+        let titleView = TitleView()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        return titleView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTitleView()
     }
+    
+    private func configureTitleView() {
+        view.addSubview(titleView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        titleView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        titleView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        titleView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+    }
+    
 }
