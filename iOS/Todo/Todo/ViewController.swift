7515f2417346093001c1784acc0034686d52bc82
ehgud0670 <kimdo2297@gmail.com>
2020-04-06 19:51:15 +0900

    chore: todo 프로젝트 생성하고 스토리보드의 디바이스를 ipad 11 인치로 변경함

---
+++ iOS/Todo/Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by kimdo2297 on 2020/04/06.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
