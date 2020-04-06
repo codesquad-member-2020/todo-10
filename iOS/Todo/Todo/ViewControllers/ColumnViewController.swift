7f6b09a97b9f96e272a61dd9c417f4b617ec0c06
ehgud0670 <kimdo2297@gmail.com>
2020-04-06 20:58:44 +0900

    [feat] ColumnViewController를 생성해서 스토리보드의 각 Column 뷰컨에 지정함

---
+++ iOS/Todo/Todo/ViewControllers/ColumnViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ColumnViewController.swift
+//  Todo
+//
+//  Created by kimdo2297 on 2020/04/06.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class ColumnViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
