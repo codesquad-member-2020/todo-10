66fb9ae8cc27703de75f886f4d0f9327e781b3e3
ehgud0670 <kimdo2297@gmail.com>
2020-04-06 22:37:10 +0900

    feat: 타이틀 뷰 안에 라벨과 추가 버튼을 추가하고 오토레이아웃을 적용함

--- iOS/Todo/Todo/Views/TitleView.swift
+++ iOS/Todo/Todo/Views/TitleView.swift
@@ -9,5 +9,50 @@
 import UIKit
 
 final class TitleView: UIView {
+    let badge: UILabel = {
+        let badge = UILabel()
+        badge.translatesAutoresizingMaskIntoConstraints = false
+        badge.widthAnchor.constraint(equalTo: badge.heightAnchor, multiplier: 1).isActive = true
+        badge.layer.cornerRadius = badge.font.pointSize * 0.60
+        badge.layer.masksToBounds = true
+        badge.text = "1"
+        badge.textAlignment = .center
+        badge.backgroundColor = .white
+        return badge
+    }()
+    
+    let plusButton: UIButton = {
+        let plusButton = UIButton()
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.tintColor = .black
+        plusButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        return plusButton
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureBadge()
+        configurePlusButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureBadge()
+        configurePlusButton()
+    }
+    
+    private func configureBadge() {
+        addSubview(badge)
+        badge.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        badge.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        badge.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    private func configurePlusButton() {
+        addSubview(plusButton)
+        plusButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        plusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        plusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
     
 }
