1569dbb40254bd531c90d0ea06c890b8c67753a2
ehgud0670 <kimdo2297@gmail.com>
2020-04-06 21:18:29 +0900

    refactor: UITableViewController에서 UIViewController를 상속받은 뷰컨으로 다시 설정함

---
+++ iOS/Todo/Todo/ViewControllers/AcitivitiyLogViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AcitivitiyLogViewController.swift
+//  Todo
+//
+//  Created by kimdo2297 on 2020/04/06.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class AcitivitiyLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
