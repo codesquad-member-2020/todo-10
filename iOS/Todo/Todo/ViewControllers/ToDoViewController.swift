fd974628754c6e815914059cff2ad8a44e47f16f
ehgud0670 <kimdo2297@gmail.com>
2020-04-07 20:01:04 +0900

    feat: prepare 메소드를 오버라이드하여 segue와 연결된 카드리스트 뷰컨들에게 제목을 부여함

--- iOS/Todo/Todo/ViewControllers/ToDoViewController.swift
+++ iOS/Todo/Todo/ViewControllers/ToDoViewController.swift
@@ -9,8 +9,30 @@
 import UIKit
 
 final class ToDoViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    enum SegueIdentifier: String {
+        case toDo
+        case inProgress
+        case done
+        
+        func isValid(identifier: String?, completionHandler: () -> ()) {
+            if identifier == self.rawValue {
+                completionHandler()
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        SegueIdentifier.toDo.isValid(identifier: segue.identifier) {
+            guard let toDoViewController = segue.destination as? CardListController else { return }
+            toDoViewController.setTitle(title: TitleLabel.Title.toDo)
+        }
+        SegueIdentifier.inProgress.isValid(identifier: segue.identifier) {
+            guard let inProgressViewController = segue.destination as? CardListController else { return }
+            inProgressViewController.setTitle(title: TitleLabel.Title.inProgress)
+        }
+        SegueIdentifier.done.isValid(identifier: segue.identifier) {
+            guard let doneViewController = segue.destination as? CardListController else { return }
+            doneViewController.setTitle(title: TitleLabel.Title.done)
+        }
     }
 }
-
