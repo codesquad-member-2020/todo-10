9a13d0e09b08b11d3b2cffa69a46c9ced189f4d6
ehgud0670 <kimdo2297@gmail.com>
2020-04-07 10:33:42 +0900

    refactor: 요구사항에 맞게 popover의 VC를 UITableViewController를 상속하는 VC로 재설정함

--- iOS/Todo/Todo/ViewControllers/AcitivitiyLogController.swift
+++ iOS/Todo/Todo/ViewControllers/AcitivitiyLogController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class AcitivitiyLogController: UIViewController {
+final class AcitivitiyLogController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
