4581bf498a3c3e88574aaabe9a9b2f0177a6a88c
ehgud0670 <kimdo2297@gmail.com>
2020-04-07 20:36:02 +0900

    style: 메인이라는 이름이 더 적절하여 이름 변경함

--- iOS/Todo/Todo/ViewControllers/MainViewController.swift
+++ iOS/Todo/Todo/ViewControllers/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class ToDoViewController: UIViewController {
+final class MainViewController: UIViewController {
     enum SegueIdentifier: String {
         case toDo
         case inProgress
