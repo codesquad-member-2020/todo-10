d473cf6714eaafc2b83fb960c972b577955929c7
baekCode <sosaer88@gmail.com>
2020-04-07 12:57:31 +0900

    [#2]style: webpack scss

--- FE/src/js/main.js
+++ FE/src/js/main.js
@@ -1,1 +1,4 @@
 // import '../style/common.css';
+// import '../style/todo.css';
+// import '../style/card.css';
+// import '../style/menu.css';
--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 module.exports = {
-    mode: "development",
-    entry: "./src/js/main.js",
+    mode: 'development',
+    entry: ['./src/js/main.js', './src/style/common.scss','./src/style/todo.scss','./src/style/card.scss','./src/style/menu.scss'],
     output: {
-        path: path.resolve(__dirname, "src/dist"),
-        filename: "bundle.js",
+        path: path.resolve(__dirname, 'src/dist'),
+        filename: 'bundle.js',
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader','sass-loader'],
-            }
-        ]
-    }
-}
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
