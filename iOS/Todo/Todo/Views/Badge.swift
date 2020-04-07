ee8b728f5f381648d471976c065c74dff64c0d2a
ehgud0670 <kimdo2297@gmail.com>
2020-04-07 11:06:24 +0900

    refactor: UILabel를 커스팅마이징하여 벳지객체를 만듬

---
+++ iOS/Todo/Todo/Views/Badge.swift
@@ -0,0 +1,47 @@
+//
+//  Badge.swift
+//  Todo
+//
+//  Created by kimdo2297 on 2020/04/07.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+class Badge: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        configureAspectRatio()
+        configureCornerRadius()
+        configureText()
+        configureBackgroundColor()
+    }
+
+    private func configureAspectRatio() {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalTo: heightAnchor, multiplier: 1).isActive = true
+    }
+    
+    private func configureCornerRadius() {
+        layer.cornerRadius = font.pointSize * 0.65
+        layer.masksToBounds = true
+    }
+    
+    private func configureText() {
+        text = "1"
+        textAlignment = .center
+    }
+    
+    private func configureBackgroundColor() {
+        backgroundColor = .white
+    }
+}
