e720d8f781bf3139cb75909a6e8f2ceff42a481d
sgdg <seungdeng17@gmail.com>
2020-04-08 16:23:26 +0900

    [#32] modify : events 폴더 이름 수정(eventHandles), 불필요한 소스 파일 제거

--- FE/src/ts/util/commonUtil.ts
+++ FE/src/ts/util/commonUtil.ts
@@ -1,22 +1,21 @@
-function getElement(target: string) {
+function getEl(target: string) {
     return document.querySelector(target);
 }
-function getElements(target: string) {
+
+function getEls(target: string) {
     return document.querySelectorAll(target);
 }
+
+function getParentEl(el: HTMLElement, target: string) {
+    return el.closest(target);
+}
+
 function addClass(target: HTMLElement, className: string) {
     target.classList.add(className);
 }
+
 function removeClass(target: HTMLElement, className: string) {
     target.classList.remove(className);
 }
-function clearTransition(target: HTMLElement) {
-    target.style.transition = '0s step-start';
-}
-const getData = async (url: string) => {
-    const response = await fetch(url);
-    const resPromise = await response.json();
-    return resPromise;
-};
 
-export { getElement, getElements, addClass, removeClass, clearTransition, getData };
+export { getEl, getEls, getParentEl, addClass, removeClass };
