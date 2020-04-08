179c2d18626fb67107250feb67f32b269cd57d25
Jay <beginin15@gmail.com>
2020-04-08 20:54:44 +0900

    [#26] feat: 카드 수정, 삭제 Mockup API 구현
    - delete하는 경우, delete가 발생된 Section의 카드 리스트를 반환함. 도메인을 반환하지 않고 아이디 값을 가지고 있는 JSONNode 객체를 DB 내에 저장된 순서의 리스트를 반환

--- BE/src/main/java/com/codesquad/team10/todo/api/mock/MockCardApiController.java
+++ BE/src/main/java/com/codesquad/team10/todo/api/mock/MockCardApiController.java
@@ -2,14 +2,20 @@ package com.codesquad.team10.todo.api.mock;
 
 import com.codesquad.team10.todo.bean.ResponseData;
 import com.codesquad.team10.todo.domain.Card;
-import com.codesquad.team10.todo.domain.Route;
+import com.codesquad.team10.todo.domain.User;
+import com.codesquad.team10.todo.exception.ResourceNotFoundException;
+import com.codesquad.team10.todo.exception.UserNotFoundException;
+import com.codesquad.team10.todo.repository.MockUserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,16 +23,61 @@ import java.util.Map;
 public class MockCardApiController {
 
     private static final Logger log = LoggerFactory.getLogger(MockCardApiController.class);
-    private static long seq = 0;
+
+    @Autowired
+    private MockUserRepository mockUserRepository;
+    private static int seq = 0;
 
     @PostMapping("")
-    public ResponseEntity<ResponseData> create(@PathVariable long sectionId, @RequestBody Card card) {
-
+    public ResponseEntity<ResponseData> create(@PathVariable int sectionId, @RequestBody Card card) {
         log.debug("sectionId: {}, card: {}", sectionId, card);
 
-        card.setId(++seq);
-        card.setCreatedDateTime(LocalDateTime.now());
-        card.setUpdatedDateTime(LocalDateTime.now());
+        User dbUser = mockUserRepository.findByEmail().orElseThrow(UserNotFoundException::new);
+        try {
+            dbUser.addCard(sectionId, card);
+            card.setId(seq++);
+            card.setCreatedDateTime(LocalDateTime.now());
+            card.setUpdatedDateTime(LocalDateTime.now());
+        } catch (IndexOutOfBoundsException e) {
+            throw new ResourceNotFoundException();
+        }
         return new ResponseEntity<>(new ResponseData(ResponseData.Status.SUCCESS, card), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponseData> update(@PathVariable int sectionId, @PathVariable int id,
+                                               @RequestBody Map<String, String> requestBody) {
+        String title = requestBody.get("title");
+        String content = requestBody.get("content");
+
+        log.debug("sectionId: {}, card id: {}, title: {}, content: {}", sectionId, id, title, content);
+
+        User dbUser = mockUserRepository.findByEmail().orElseThrow(UserNotFoundException::new);
+        try {
+            dbUser.updateCard(sectionId, id, title, content);
+        } catch (NullPointerException | IndexOutOfBoundsException e) {
+            throw new ResourceNotFoundException();
+        }
+        return new ResponseEntity<>(new ResponseData(ResponseData.Status.SUCCESS, "Card Updated"), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseData> delete(@PathVariable int sectionId, @PathVariable int id) {
+        User dbUser = mockUserRepository.findByEmail().orElseThrow(UserNotFoundException::new);
+        List<JsonNode> orders = null;
+        try {
+            orders = dbUser.deleteCard(sectionId, id);
+        } catch (IndexOutOfBoundsException e) {
+            throw new ResourceNotFoundException();
+        }
+
+        log.debug("orders: {}", orders);
+
+        return new ResponseEntity<>(new ResponseData(ResponseData.Status.SUCCESS, orders), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseData handleResourceNotFoundException(ResourceNotFoundException e) {
+        return new ResponseData(ResponseData.Status.ERROR, e.getMessage());
     }
 }
