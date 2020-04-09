8558981796c11ec510bea01947660fda20d67ddd
Jay <beginin15@gmail.com>
2020-04-09 20:19:25 +0900

    [#9] chore: mysql 서버에 연결
    - schema.sql 읽어서 스키마 생성
    - 실행 환경별 사용하는 데이터베이스 구분
    - DB Connection 테스트 코드 작성

---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS log;
+
+CREATE TABLE IF NOT EXISTS user (
+    id bigint auto_increment primary key ,
+    email varchar(64) unique not null ,
+    password varchar(64) not null
+);
+
+CREATE TABLE IF NOT EXISTS section (
+    id bigint auto_increment primary key ,
+    title varchar(100) not null ,
+    create_date_time timestamp not null default current_timestamp ,
+    update_date_time timestamp not null default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS card (
+  id bigint auto_increment primary key ,
+  user bigint references user(id) ,
+  section bigint references section(id) ,
+  title varchar(100) ,
+  content varchar(500) not null ,
+  create_date_time timestamp not null default current_timestamp ,
+  update_date_time timestamp not null default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS log (
+  id bigint auto_increment primary key ,
+  user bigint references user(id) ,
+  action enum('added', 'updated', 'moved', 'removed') not null ,
+  type enum('section', 'card') not null ,
+  source varchar(500) not null ,
+  destination varchar(500)
+);
