a3f3a7a82261dda8c28d44771603b5076e246e55
sgdg <seungdeng17@gmail.com>
2020-04-09 12:08:36 +0900

    delete : 불필요한 js 파일 ignore 처리

--- FE/src/js/eventHandles/column.js
+++
@@ -1,6 +0,0 @@
-function columnClickHandle(target) {
-    if (!target.classList.contains('toggle-form'))
-        return;
-    target.closest('.content-wrap').querySelector('.todo-form').classList.toggle('active');
-}
-export { columnClickHandle, };
