91529bfe80ad7f1faa2517885c3ed27bd23c973d
ehgud0670 <kimdo2297@gmail.com>
2020-04-10 15:26:50 +0900

    [#56] feat: 제목이 입력될 수 있도록 기능 구현함

--- iOS/Todo/Todo/ViewControllers/PlusCardViewController.swift
+++ iOS/Todo/Todo/ViewControllers/PlusCardViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 final class PlusCardViewController: UIViewController {
     private let cancelButton = CancelButton()
     private let createButton = CreateButton()
+    private let titleField = TitleField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         configureCreateButton()
         configureCancelButton()
+        configureTitleField()
     }
     
     private func configureView() {
@@ -35,6 +37,13 @@ final class PlusCardViewController: UIViewController {
         let constant: CGFloat = 27
         cancelButton.topAnchor.constraint(equalTo: view.topAnchor, constant: constant).isActive = true
         cancelButton.trailingAnchor.constraint(equalTo: createButton.leadingAnchor, constant: -constant).isActive = true
+    }
+    
+    private func configureTitleField() {
+        view.addSubview(titleField)
+        let constant: CGFloat = 27
+        titleField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: constant).isActive = true
+        titleField.topAnchor.constraint(equalTo: cancelButton.bottomAnchor, constant: constant).isActive = true
     }
 }
 
@@ -78,5 +87,37 @@ final class CreateButton: UIButton {
         imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
         tintColor = .systemBlue
         translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+final class TitleField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        font = UIFont.boldSystemFont(ofSize: 30)
+        placeholder = "Title"
+    }
+ 
+    private static let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: TitleField.padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: TitleField.padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: TitleField.padding)
     }
 }
