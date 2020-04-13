92617bb2375f79644e7c3fb06f1cf25ad800a323
Jay <beginin15@gmail.com>
2020-04-13 16:39:33 +0900

    [#88] refactor: DB 스키마 변경
    - 모든 테이블의  id 데이터 타입을 bigint => INTEGER로 변경
    - board 테이블 추가하여 참조 관계 수정함.
    - user가 board의 외래키를 가지고 있음. section이 board의 외래키를 가지고 있음.
    - log 테이블은 user가 아닌 board를 외래키로 참조하도록 변경. user는 DB 컬럼으로 관리하지 않고 값으로 가지고 있도록 할 예정

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,4 +1,8 @@
-INSERT INTO user (email, password) VALUES ('nigayo@ggmail.com', '1234');
-INSERT INTO section (user, title, create_date_time) VALUES (1, '해야할 일', CURRENT_TIMESTAMP());
-INSERT INTO section (user, title, create_date_time) VALUES (1, '하고 있는 일', CURRENT_TIMESTAMP());
-INSERT INTO section (user, title, create_date_time) VALUES (1, '완료된 일', CURRENT_TIMESTAMP());
+INSERT INTO board (title) values ('TODO 서비스');
+INSERT INTO user (email, password, board) VALUES ('nigayo', '1234', 1);
+INSERT INTO section (title, create_date_time, board) VALUES ('해야할 일', CURRENT_TIMESTAMP(), 1);
+INSERT INTO section (title, create_date_time, board) VALUES ('하고 있는 일', CURRENT_TIMESTAMP(), 1);
+INSERT INTO section (title, create_date_time, board) VALUES ('완료된 일', CURRENT_TIMESTAMP(), 1);
+INSERT INTO card (title, content, create_date_time, user, section) VALUES ('래해야', 'dfdf', CURRENT_TIMESTAMP(), 1, 1);
+INSERT INTO card (title, content, create_date_time, user, section) VALUES ('됨하는', 'dfdf', CURRENT_TIMESTAMP(), 1, 1);
+-- INSERT INTO card (title, content, create_date_time, user, section) VALUES ('중다', 'dfdf', CURRENT_TIMESTAMP(), 1, 3);
