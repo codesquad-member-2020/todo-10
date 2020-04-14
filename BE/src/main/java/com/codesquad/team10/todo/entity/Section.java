18091771d300cc9732f659b6167c67b061d01b3b
Jay <beginin15@gmail.com>
2020-04-14 14:25:26 +0900

    [#88] refactor: DB 스키마 변경
    - Board를 루트로 했을 때 insert 오류가 발생함. Section과 Board를 N:1관계로 변경하여 루트를 Section으로 변경
    - board도 Board와 N:1관계로 변경함.
    - User와 Board는 1:N유지
    - Log 테이블은 target을 enum으로 변경하여 로그 대상이 섹션인지 카드인지 구분

---
+++ BE/src/main/java/com/codesquad/team10/todo/entity/Section.java
@@ -0,0 +1,84 @@
+package com.codesquad.team10.todo.entity;
+
+import com.codesquad.team10.todo.vo.SectionDTO;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Section {
+
+    @Id
+    private Integer id;
+
+    private String title;
+
+    private LocalDateTime createDateTime;
+
+    private LocalDateTime updateDateTime;
+
+    private boolean deleted;
+
+    private List<Card> cards;
+
+    private Integer board;
+
+    public Section(Integer id, String title, LocalDateTime createDateTime, LocalDateTime updateDateTime, boolean deleted, List<Card> cards, Integer board) {
+        this.id = id;
+        this.title = title;
+        this.createDateTime = createDateTime;
+        this.updateDateTime = updateDateTime;
+        this.deleted = deleted;
+        this.cards = cards;
+        this.board = board;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public LocalDateTime getUpdateDateTime() {
+        return updateDateTime;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Integer getBoard() {
+        return board;
+    }
+
+    public void addCard(Card newCard) {
+        cards.add(newCard);
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", createDateTime=" + createDateTime +
+                ", updateDateTime=" + updateDateTime +
+                ", deleted=" + deleted +
+                ", cards=" + cards +
+                ", board=" + board +
+                '}';
+    }
+}
