bd6ccd93e8bc88535da3f34a43a028a766f02ea2
ehgud0670 <kimdo2297@gmail.com>
2020-04-14 15:30:37 +0900

    [#90] refactor: columnViewController를 만드는 함수를 만들어서 분리함 		이유: 메소드는 하나의 역할만 해야 한다.

--- iOS/Todo/Todo/ViewControllers/MainViewController.swift
+++ iOS/Todo/Todo/ViewControllers/MainViewController.swift
@@ -29,21 +29,24 @@ final class MainViewController: UIViewController {
         ColumnsUseCase.makeColumns(with: NetworkManager()) { columnsDataSource in
             columnsDataSource?.iterateColumns(with: { column in
                 DispatchQueue.main.async {
-                    let columnViewController: ColumnViewController = {
-                        let controller = ColumnViewController()
-                        controller.column = column
-                        return controller
-                    }()
-                    self.addColumnViewController(columnViewController: columnViewController)
+                    self.addColumnViewController(columnViewController: self.columnViewController(column: column))
                 }
             })
         }
     }
     
+    private func columnViewController(column: Column) -> ColumnViewController {
+        let columnViewController: ColumnViewController = {
+            let controller = ColumnViewController()
+            controller.column = column
+            return controller
+        }()
+        return columnViewController
+    }
+    
     private func addColumnViewController(columnViewController: ColumnViewController) {
         addChild(columnViewController)
         columScrollView.columnStackView.addArrangedSubview(columnViewController.view)
-        
         columnViewController.view.translatesAutoresizingMaskIntoConstraints = false
         columnViewController.view.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.32).isActive = true
         columnViewController.view.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1).isActive = true
