c04ac4e926b41bb8223bad4f1497bf8a0e1653e0
Jay <beginin15@gmail.com>
2020-04-15 03:19:24 +0900

    [#66] feat: 보드 내 이벤트 발생 시 로그 객체 생성 및 로그 테이블에 저장 구현
    - /logs 요청에 대해서 현재 boardId에 해당되는 로그 레코드를 반환하도록 구현 (로그와 보드 테이블은 참조 관계)
    - 로그 객체 반환 시, 기본 생성자가 존재하지 않으면 에러 발생. (레포지토리에서 find 하는 과정에서 필요한 듯?)

--- BE/src/main/java/com/codesquad/team10/todo/repository/LogRepository.java
+++ BE/src/main/java/com/codesquad/team10/todo/repository/LogRepository.java
@@ -1,6 +1,15 @@
 package com.codesquad.team10.todo.repository;
 
 import com.codesquad.team10.todo.entity.Log;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LogRepository extends CrudRepository<Log, Integer> {}
+import java.util.List;
+
+public interface LogRepository extends CrudRepository<Log, Integer> {
+
+    @Query("SELECT *" +
+            " FROM log l" +
+            " WHERE l.board = :boardId")
+    List<Log> findByBoardId(Integer boardId);
+}
