28b9bf280c4056fd73b675ac1848076bccdd41d0
ehgud0670 <kimdo2297@gmail.com>
2020-04-14 15:56:23 +0900

    [#90] refactor: Foundation 은 UIKit에서 포함하고 있으므로 import 문 삭제함

--- iOS/Todo/Todo/Delegates/ColumnTableDelegate.swift
+++ iOS/Todo/Todo/Delegates/ColumnTableDelegate.swift
@@ -5,7 +5,6 @@
 //  Created by kimdo2297 on 2020/04/07.
 //  Copyright © 2020 Jason. All rights reserved.
 //
-import Foundation
 import UIKit
 
 final class ColumnTableDelegate: NSObject, UITableViewDelegate {
--- iOS/Todo/Todo/ViewControllers/ColumnViewController.swift
+++ iOS/Todo/Todo/ViewControllers/ColumnViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+
 final class ColumnViewController: UIViewController {
     //MARK:- internal property
     private let titleView = TitleView()
