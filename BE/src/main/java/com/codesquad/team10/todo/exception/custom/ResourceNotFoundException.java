aef0f747b768f090cacf311e9d09ab8ebd88e00b
Jay <beginin15@gmail.com>
2020-04-15 00:11:03 +0900

    [#66] feat: 요청 데이터가 충족하지 못한 경우 발생하는 커스텀 예외 클래스 정의
    - InvalidRequestException : 특정 api를 호출할 때 필요한 요청 데이터가 포함되지 않은 경우 발생시킴

--- BE/src/main/java/com/codesquad/team10/todo/exception/custom/ResourceNotFoundException.java
+++ BE/src/main/java/com/codesquad/team10/todo/exception/custom/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.codesquad.team10.todo.exception.custom;
 import com.codesquad.team10.todo.constants.ExceptionMessage;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException() {
         super(ExceptionMessage.RESOURCE_NOT_FOUND.getMessage());
     }
