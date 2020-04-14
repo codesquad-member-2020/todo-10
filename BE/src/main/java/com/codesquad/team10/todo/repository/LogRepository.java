389c8a357b58253391b499ced5fa6447cd4b558c
Jay <beginin15@gmail.com>
2020-04-14 14:27:25 +0900

    [#88] feat: 카드, 로그, 섹션 레포지토리 생성
    - 루트인 Board로 CRUD를 수행하려고 했으나 insert 오류가 발생하여 각 레포지토리를 생성함.
    - Board 레포는 맨 처음 로그인 할 때 검색용으로 사용. (현재 보드에 포함되어 있는 유저인지)
    - 단, 카드 객체는 Section 레포를 통해서 추가하도록 구현. Section과 Card는 1:N 관계 유지

---
+++ BE/src/main/java/com/codesquad/team10/todo/repository/LogRepository.java
@@ -0,0 +1,6 @@
+package com.codesquad.team10.todo.repository;
+
+import com.codesquad.team10.todo.entity.Log;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LogRepository extends CrudRepository<Log, Integer> {}
