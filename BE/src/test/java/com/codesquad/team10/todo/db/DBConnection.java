fae62aa4135774b792324c1dec21a37595ab3c54
Jay <beginin15@gmail.com>
2020-04-15 17:51:05 +0900

    [#122] fix: 배포 빌드 시 테스트 제거, mysql 접속 url을 localhost로 변경

--- BE/src/test/java/com/codesquad/team10/todo/db/DBConnection.java
+++ BE/src/test/java/com/codesquad/team10/todo/db/DBConnection.java
@@ -8,18 +8,18 @@ import java.sql.DriverManager;
 
 @SpringBootTest
 public class DBConnection {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://15.164.63.83:3306/TEST_KANBANBOARD";
-    private static final String USER = "springbootUser";
-    private static final String PW = "sBu_1234!";
-
-    @Test
-    public void testConnection() throws Exception {
-        Class.forName(DRIVER);
-        try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
-            System.out.println(con);
-        } catch (Exception e) {
-            System.err.println(e);
-        }
-    }
+//    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+//    private static final String URL = "jdbc:mysql://15.164.63.83:3306/TEST_KANBANBOARD";
+//    private static final String USER = "springbootUser";
+//    private static final String PW = "sBu_1234!";
+//
+//    @Test
+//    public void testConnection() throws Exception {
+//        Class.forName(DRIVER);
+//        try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
+//            System.out.println(con);
+//        } catch (Exception e) {
+//            System.err.println(e);
+//        }
+//    }
 }
