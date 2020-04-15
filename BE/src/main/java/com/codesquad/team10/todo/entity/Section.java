54b2d107524704fe1b3c2d250fb716edfeb1e0ef
Jay <beginin15@gmail.com>
2020-04-15 19:59:44 +0900

    [#117] chore: 불필요한 import 제거

--- BE/src/main/java/com/codesquad/team10/todo/entity/Section.java
+++ BE/src/main/java/com/codesquad/team10/todo/entity/Section.java
@@ -1,7 +1,6 @@
 package com.codesquad.team10.todo.entity;
 
 import com.codesquad.team10.todo.exception.custom.UnmatchedRequestDataException;
-import com.codesquad.team10.todo.vo.CardDTO;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
