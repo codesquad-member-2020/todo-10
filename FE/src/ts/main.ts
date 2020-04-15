a50bb277b76c27577a82a8f7e6e2d17c232166ab
baekCode <sosaer88@gmail.com>
2020-04-15 23:21:18 +0900

    [#100] feat: 로그인 임시 구현

--- FE/src/ts/main.ts
+++ FE/src/ts/main.ts
@@ -1,3 +1,5 @@
+import { getEl, addClass } from './utils/commonUtil';
+import { USER } from './loginModule/data';
 import TodoController from './todoModules/todoController';
 import TodoView from './todoModules/todoView';
 import TodoEventManager from './todoModules/todoEventManager';
@@ -6,4 +8,34 @@ const todoView = new TodoView();
 const todoEventManager = new TodoEventManager(todoView);
 const todoController = new TodoController(todoView, todoEventManager);
 
-todoController.runTodoApp();
+const dimmedEl = getEl('#dimmed');
+addClass(dimmedEl, 'active');
+
+const drawLoading = () => {
+    return `<div class="loading">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>`;
+};
+const checkUser = (data) => {
+    const checkEmail = USER.find((user) => user.email === data.email);
+    if (!checkEmail) return alert('회원이 아닙니다.');
+    if (checkEmail.password !== data.password) return alert('비밀번호를 확인해주세요.');
+    return data;
+};
+
+dimmedEl.querySelector('.btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const loginIdEl = <HTMLInputElement>getEl('#userID');
+    const loginPwEl = <HTMLInputElement>getEl('#password');
+    const data = {
+        email: loginIdEl.value,
+        password: loginPwEl.value,
+    };
+    const checkEmail = USER.find((user) => user.email === data.email);
+    if (!checkEmail) return alert('회원이 아닙니다.');
+    if (checkEmail.password !== data.password) return alert('비밀번호를 확인해주세요.');
+    dimmedEl.innerHTML = drawLoading();
+    todoController.runTodoApp(data);
+});
--- FE/src/ts/todoModules/todoController.ts
+++ FE/src/ts/todoModules/todoController.ts
@@ -1,3 +1,4 @@
+import { getEl, removeClass } from '../utils/commonUtil';
 import { httpRequest } from '../utils/httpRequestUtil';
 import { URL } from '../contants/url';
 import TodoView from './todoView';
@@ -6,15 +7,16 @@ import TodoEventManager from './todoEventManager';
 class TodoController {
     private todoView: TodoView;
     private todoEventManager: TodoEventManager;
-    
-    constructor( todoView:TodoView, todoEventManager:TodoEventManager ) {
+
+    constructor(todoView: TodoView, todoEventManager: TodoEventManager) {
         this.todoView = todoView;
         this.todoEventManager = todoEventManager;
     }
 
-    runTodoApp(): void {
+    runTodoApp(data): void {
         const url = `${URL.MOCKUP.BASE}/mock/login`;
-        httpRequest.login(url).then(todoData => {
+        httpRequest.login(url, data).then((todoData) => {
+            removeClass(getEl('#dimmed'), 'active');        
             this.todoView.todoAppRender(todoData);
             this.todoView.todoModalRender();
             this.todoEventManager.todoAppEventInit();
