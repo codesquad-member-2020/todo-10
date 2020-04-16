5ef48a769c41f9a2f9f720e37224856da845de1e
Jay <beginin15@gmail.com>
2020-04-16 21:25:05 +0900

    [#69] chore: 초기 섹션은 3개로 생성하도록 data.sql 수정

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -5,4 +5,3 @@ INSERT INTO user (name, password, board, board_key) VALUES ('nigayo', '1234', 1,
 INSERT INTO section (title, create_date_time, board) VALUES ('해야할 일', CURRENT_TIMESTAMP(), 1);
 INSERT INTO section (title, create_date_time, board) VALUES ('하고 있는 일', CURRENT_TIMESTAMP(), 1);
 INSERT INTO section (title, create_date_time, board) VALUES ('완료된 일', CURRENT_TIMESTAMP(), 1);
-INSERT INTO section (title, create_date_time, board) VALUES ('사라진 일', CURRENT_TIMESTAMP(), 1);
