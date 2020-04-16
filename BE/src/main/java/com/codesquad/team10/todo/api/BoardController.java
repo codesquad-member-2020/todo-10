b59fc485718b0ce657a07193bc432cb80380617a
Jay <beginin15@gmail.com>
2020-04-17 00:16:22 +0900

    [#69] refactor: 토큰 인증 로직을 인터셉터에서 처리, 각종 컨트롤러의 중복 코드 제거
    - 토큰 검증이 필요한 메소드에 존재하는 중복코드 제거

--- BE/src/main/java/com/codesquad/team10/todo/api/BoardController.java
+++ BE/src/main/java/com/codesquad/team10/todo/api/BoardController.java
@@ -1,24 +1,15 @@
 package com.codesquad.team10.todo.api;
 
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.codesquad.team10.todo.entity.Section;
-import com.codesquad.team10.todo.exception.custom.ForbiddenException;
-import com.codesquad.team10.todo.exception.custom.InvalidTokenException;
-import com.codesquad.team10.todo.exception.custom.UnauthorizedException;
-import com.codesquad.team10.todo.exception.custom.UserNotFoundException;
 import com.codesquad.team10.todo.repository.SectionRepository;
-import com.codesquad.team10.todo.repository.UserRepository;
 import com.codesquad.team10.todo.response.ResponseData;
 import com.codesquad.team10.todo.entity.User;
 import com.codesquad.team10.todo.constants.ResponseMessage;
-import com.codesquad.team10.todo.util.JWTUtils;
+import com.codesquad.team10.todo.service.AuthService;
 import com.codesquad.team10.todo.util.ModelMapper;
-import com.codesquad.team10.todo.dto.CardDTO;
 import com.codesquad.team10.todo.dto.SectionDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/board")
@@ -35,43 +25,30 @@ public class BoardController {
 
     private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
-    private UserRepository userRepository;
+    private AuthService authService;
     private SectionRepository sectionRepository;
 
-    public BoardController(UserRepository userRepository, SectionRepository sectionRepository) {
-        this.userRepository = userRepository;
+    public BoardController(AuthService authService, SectionRepository sectionRepository) {
+        this.authService = authService;
         this.sectionRepository = sectionRepository;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseData> login(@RequestBody User loginUser, HttpServletResponse response) {
-        User dbUser = userRepository.findByName(loginUser.getName()).orElseThrow(UserNotFoundException::new);
-        if (!dbUser.confirmPassword(loginUser))
-            throw new UnauthorizedException();
+    public ResponseEntity<ResponseData> login(@RequestBody User loginUser,
+                                              HttpServletResponse response) {
+        if (authService.verifyUser(loginUser, response))
+            return new ResponseEntity<>(new ResponseData(ResponseData.Status.SUCCESS, ResponseMessage.LOGIN_SUCCESS.getMessage()), HttpStatus.OK);
 
-        logger.debug("board id: {}", dbUser.getBoard());
-
-        String token = JWTUtils.createTokenByUser(dbUser);
-        response.setHeader(HttpHeaders.AUTHORIZATION, token);
-        return new ResponseEntity<>(new ResponseData(ResponseData.Status.SUCCESS, ResponseMessage.LOGIN_SUCCESS.getMessage()), HttpStatus.OK);
+        // 비밀번호 틀린 경우
+        return new ResponseEntity<>(new ResponseData(ResponseData.Status.ERROR, ResponseMessage.LOGIN_FAILED.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @GetMapping("")
     public ResponseEntity<ResponseData> show(HttpServletRequest request) {
-        User userData = null;
-        try {
-            userData = JWTUtils.getUserFromJWT(request.getHeader(HttpHeaders.AUTHORIZATION));
-        } catch (SignatureVerificationException | NullPointerException e) {
-            throw new ForbiddenException();
-        } catch (JWTDecodeException e) {
-            throw new InvalidTokenException();
-        }
+        User loginUser = (User)request.getAttribute("user");
         List<SectionDTO> sectionDTOS = new ArrayList<>();
-        List<Section> sections = sectionRepository.findByBoardId(userData.getBoard());
+        List<Section> sections = sectionRepository.findByBoardId(loginUser.getBoard());
         for (Section section : sections) {
-            List<CardDTO> cards = section.getCards().stream()
-                    .map(card -> (CardDTO)ModelMapper.of(card))
-                    .collect(Collectors.toList());
             SectionDTO sectionDTO = (SectionDTO)ModelMapper.of(section);
             sectionDTOS.add(sectionDTO);
         }
