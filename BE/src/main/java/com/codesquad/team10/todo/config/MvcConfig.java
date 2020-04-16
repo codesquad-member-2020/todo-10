2f61751b191358463ffc17d26a832d094d036e30
Jay <beginin15@gmail.com>
2020-04-16 17:28:20 +0900

    [#153] fix: filter, interceptor에 헤더 설정
    - Authorization 헤더에 담긴 value에 저장된 값을 브라우저에서 가져올 수 있어야 함 (테스트 필요)

---
+++ BE/src/main/java/com/codesquad/team10/todo/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.codesquad.team10.todo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier(value = "authInterceptor")
+    private HandlerInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/board/**");
+    }
+
+}
