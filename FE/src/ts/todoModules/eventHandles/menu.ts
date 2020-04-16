bad15055c3687441087ccc68b47c37db79281078
sgdg <seungdeng17@gmail.com>
2020-04-16 13:05:38 +0900

    [#101] modify : 메뉴 버튼을 눌렀을 때만 창이 나오도록 수정

--- FE/src/ts/todoModules/eventHandles/menu.ts
+++ FE/src/ts/todoModules/eventHandles/menu.ts
@@ -1,7 +1,8 @@
 import { COMMON_RULE } from '../../contants/constant';
 import { addClass, removeClass, hasClass } from '../../utils/commonUtil';
 
-function showMenu() {
+function showMenu({ target }) {
+    if (!hasClass(target, 'toggle-menu')) return;
     addClass(this.todoView.todoMenu, COMMON_RULE.ACTIVE_KEY);
 }
 
