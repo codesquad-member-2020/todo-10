9eb3f7757e59a9e2c6ae34b867075bab0154cd5b
Jay <beginin15@gmail.com>
2020-04-17 00:16:49 +0900

    [#69] chore: 테스트를 위한 회원 데이터 추가

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO board (id) values (1);
 
 INSERT INTO user (name, password, board, board_key) VALUES ('nigayo', '1234', 1, 0);
+INSERT INTO user (name, password, board, board_key) VALUES ('tododo', '1004', 1, 1);
 
 INSERT INTO section (title, create_date_time, board) VALUES ('해야할 일', CURRENT_TIMESTAMP(), 1);
 INSERT INTO section (title, create_date_time, board) VALUES ('하고 있는 일', CURRENT_TIMESTAMP(), 1);
