e82d3729cf0024eddf9a8f86061938fd6549a317
ehgud0670 <kimdo2297@gmail.com>
2020-04-17 12:14:47 +0900

    [#170] feat: 이동에 대한 응답 객체 생성함

---
+++ iOS/Todo/Todo/Models/MoveResponse.swift
@@ -0,0 +1,20 @@
+//
+//  MoveResponse.swift
+//  Todo
+//
+//  Created by kimdo2297 on 2020/04/17.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+struct MoveResponse: Codable {
+    let status: Status
+    let content: MoveContent
+}
+
+struct MoveContent: Codable {
+    let log_id: Int
+    let card_count_to_section: Int?
+    let card_count_from_section: Int
+}
